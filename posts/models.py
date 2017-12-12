23cc352c21954da2190f3a87cdb3d88a23a5d8b6
Alex Choi <alex.ji.choi@gmail.com>
2017-12-12 16:52:05 -0500

    added post and category models

--- posts/models.py
+++ posts/models.py
@@ -3,13 +3,25 @@ from django.db import models
 
 
 
+class Category(models.Model):
+	name = models.CharField(max_length=100, help_text="Category for the Bucketlist (e.g. Travel, Milestone, Skills")
+
+	def __str__(self):
+		return self.name
+
+
+
 class Post(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length=100)
-	content = models.TextField(max_length=1000)
+	description = models.TextField(max_length=1000)
+	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+	completed = models.BooleanField(default=False)
 	date_created = models.DateTimeField(auto_now_add=True)
 	date_updated = models.DateTimeField(auto_now=True)
-
+	date_completed = models.DateField(null=True, blank=True)
 
 	def __str__(self):
 		return self.title
+
+
