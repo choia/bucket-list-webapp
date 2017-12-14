a68597481cd54611a181a12968b1add836ac2aec
Alex Choi <alex.ji.choi@gmail.com>
2017-12-13 20:39:37 -0500

    added create,delete,edit class and templates

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 from . import views
 
 
+# Post URL CONF
 urlpatterns = [
 		url(r'^posts/$', views.PostListView.as_view(), name='post-home'),	
-		url(r'^posts/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post-detail'),	
+		url(r'^posts/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post-detail'),
+		url(r'^posts/create/$', views.PostCreate.as_view(), name='post-create'),
+		url(r'^posts/(?P<pk>\d+)/edit/$', views.PostEdit.as_view(), name='post-edit'),
+		url(r'^posts/(?P<pk>\d+)/delete/$', views.PostDelete.as_view(), name='post-delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -12,3 +12,18 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
 	model = Post
 	template_name = 'post_detail.html'
+
+
+class PostCreate(CreateView):
+	model = Post
+	template_name = 'post_create.html'
+
+
+class PostEdit(UpdateView):
+	model = Post
+	template_name = 'post_edit.html'
+
+
+class PostDelete(DeleteView):
+	model = Post
+	template_name = 'post_delete.html'
