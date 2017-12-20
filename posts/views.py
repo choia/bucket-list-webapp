2eac24ad5e12a3645145166c73c5d725395e3f0c
Alex Choi <alex.ji.choi@gmail.com>
2017-12-19 19:53:49 -0500

    login authenticate add-on on posts listview

--- posts/views.py
+++ posts/views.py
@@ -9,10 +9,10 @@ from .forms import PostForm
 
 
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin, ListView):
 	model = Post
-	def get_queryset(self):
-		return Post.objects.filter(completed=False)
+	def get_queryset(self):		
+		return Post.objects.filter(completed=False, user=self.request.user)
 
 	template_name = 'post_home.html'
 
