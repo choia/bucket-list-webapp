6a4d7321543553402b04ec31bf340f39ba7fa7dd
Alex Choi <alex.ji.choi@gmail.com>
2017-12-20 14:41:46 -0500

    profile detailview added

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-
 from .models import Post
 from .forms import PostForm
 
--- profiles/views.py
+++ profiles/views.py
@@ -2,15 +2,30 @@ from django.contrib.auth import get_user_model
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
+from posts.models import Post
+
 
 User = get_user_model()
 
 class ProfileDetailView(DetailView):
-	queryset = User.objects.filter(is_active=True)
+
+	queryset = User.objects.filter(is_active=True)	
 	template_name = 'profiles/user.html'
+
+
+	def get_context_data(self, **kwargs):		
+		context = super().get_context_data(**kwargs)		
+		bucket_ongoing = Post.objects.filter(completed=False, user=self.request.user).count()
+		bucket_complete = Post.objects.filter(completed=True, user=self.request.user).count()
+		context['bucket_ongoing'] = bucket_ongoing
+		context['bucket_complete'] = bucket_complete
+		return context
+
 
 	def get_object(self):
 		username = self.kwargs.get("username")
 		if username is None:
 				raise Http404
 		return get_object_or_404(User, username__iexact=username, is_active=True)
+
+
